b1854b70304ca5515245cedf5b9b968bcb36f7f3
Niclas Rosengren <niclas@niro.nu>
2024-05-02 14:54:50 +0200

    progress

--- src/jwt.rs
+++ src/jwt.rs
@@ -4,7 +4,7 @@ use rsa::{
     pkcs1v15::SigningKey,
     pkcs8::{DecodePrivateKey, DecodePublicKey, EncodePublicKey},
     sha2::{Digest, Sha256},
-    signature::{Signer, SignatureEncoding},
+    signature::{SignatureEncoding, Signer},
     RsaPrivateKey, RsaPublicKey,
 };
 
@@ -29,6 +29,8 @@ pub fn create_token(
         .duration_since(std::time::SystemTime::UNIX_EPOCH)
         .context("getting system time")?;
 
+    println!("thumbprint: {thumbprint}");
+
     let payload = Payload {
         iss: format!("{account_identifier}.{user}.SHA256:{thumbprint}"),
         sub: format!("{account_identifier}.{user}"),
@@ -39,22 +41,23 @@ pub fn create_token(
 
     // serialize payload to json
     let payload_string = serde_json::to_string(&payload).context("serializing payload")?;
+    let payload_b64 = base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(payload_string);
+    let header_b64 = base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(HEADER);
 
-    // Hash the header and payload
     let mut hasher = Sha256::new();
-    hasher.reset();
-    hasher.update(HEADER.as_bytes());
+    hasher.update(&header_b64);
     hasher.update(b".");
-    hasher.update(&payload_string);
+    hasher.update(&payload_b64);
     let digest = hasher.finalize();
     let signing_key = SigningKey::<Sha256>::new(priv_key);
     // Sign the hash with private key
 
     let hash = signing_key.sign(&digest);
 
-    let b64_hash = base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(hash.to_bytes().as_ref());
+    let b64_hash =
+        base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(hash.to_bytes().as_ref());
 
-    Ok(format!("{}.{}.{}", HEADER, payload_string, b64_hash))
+    Ok(format!("{}.{}.{}", header_b64, payload_b64, b64_hash))
 }
 
 static HEADER: &str = r#"{"alg":"RS256","typ":"JWT"}"#;
@@ -65,4 +68,34 @@ struct Payload {
     sub: String,
     iat: u128,
     exp: u128,
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn assert_token_is_correct() {
+        let private_key = std::env::var("PRIVATE_KEY_PEM")
+            .context("PRIVATE_KEY_PEM env var")
+            .expect("PRIVATE_KEY_PEM must be set");
+        let public_key = std::env::var("PUBLIC_KEY_PEM")
+            .context("PUBLIC_KEY_PEM env var")
+            .expect("PUBLIC_KEY_PEM must be set");
+
+        let host = "fx81169.eu-central-1";
+        let account_identifier = "fx81169";
+        let user = "ParkandoReader";
+
+        let token = create_token(
+            PublicKey(public_key),
+            PrivateKey(private_key),
+            account_identifier,
+            user,
+        )
+        .expect("creating token");
+
+        assert_eq!("asdsad", token);
+    }
 }
